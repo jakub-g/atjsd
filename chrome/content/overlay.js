c5fbe6ab25cb903cc27bef2d060f5439f860c9af
jakub-g <jakub.gieryluk@amadeus.com>
2013-11-28 16:19:00 +0100

    ATJSD rebrand

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -33,7 +33,7 @@
   }
 
   function update_status_text(enabled) {
-    document.getElementById("jsbStatus").label = enabled ? "JSD ON" : "JSD OFF";
+    document.getElementById("jsbStatus").label = enabled ? "ATJSD ON" : "ATJSD OFF";
   }
 
   function toggle() {
